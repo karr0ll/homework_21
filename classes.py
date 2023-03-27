b0e7a362cb84a795b0ec9b71b2c3db12fcd07dc0
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-28 00:14:48 +0300

    hints added

--- classes.py
+++ classes.py
@@ -2,95 +2,92 @@ from abc import ABC, abstractmethod
 
 
 class Storage(ABC):
-    def __init__(self, items, capacity):
-        self.items = {}
-        self.capacity = int(capacity)
 
     @abstractmethod
-    def add(self, item, quantity):
+    def add(self, item: str, quantity: int) -> None:
         pass
 
     @abstractmethod
-    def remove(self, item, quantity):
+    def remove(self, item: str, quantity: int) -> None:
         pass
 
     @abstractmethod
-    def get_free_space(self):
+    def get_free_space(self) -> int:
         pass
 
     @abstractmethod
-    def get_items(self):
+    def get_items(self) -> dict:
         pass
 
     @abstractmethod
-    def get_unique_items_count(self, items):
+    def get_unique_items_count(self, _items: dict) -> int:
         pass
 
 
 class Store(Storage):
 
     def __init__(self):
-        self.items = {}
+        self._items = {}
         self.capacity = 100
 
-    def add(self, item, quantity):
+    def add(self, item: str, quantity: int) -> None:
         """
-        добавляет новый товар в список всех товаров, или увеличивает количество уже существующего
+        Добавляет новый товар в список всех товаров, или увеличивает количество уже существующего
         :param item: название товара
         :param quantity: кол-во
         :return:
         """
-        if item in self.items.keys():
+        if item in self._items.keys():
             if 0 <= quantity <= self.get_free_space():
-                self.items[item] = self.items[item] + quantity
+                self._items[item] = self._items[item] + quantity
                 self.print_add_message(item)
             else:
                 self.print_low_capacity_message(item)
 
-        if item not in self.items.keys():
+        if item not in self._items.keys():
             if 0 <= quantity <= self.get_free_space():
-                self.items[item] = quantity
+                self._items[item] = quantity
                 self.print_add_message(item)
             else:
                 self.print_low_capacity_message(item)
 
-    def remove(self, item, quantity):
+    def remove(self, item: str, quantity: int) -> str:
         """
-        уменьшает количество товара в списке всех товаров
+        Уменьшает количество товара в списке всех товаров
         :param item: название товара
         :param quantity: кол-во
         :return:
         """
-        if self.items[item] - quantity >= 0:
-            self.items[item] = self.items[item] - quantity
+        if self._items[item] - quantity >= 0:
+            self._items[item] = self._items[item] - quantity
         else:
             return (f"недостаточно запасов товара '{item}'.\n"
-                    f"Можно переместить {self.items[item]} шт.")
+                    f"Можно переместить {self._items[item]} шт.")
 
-    def get_free_space(self):
+    def get_free_space(self) -> int:
         """
-        получает кол-во свободных мест на складе
+        Получает кол-во свободных мест на складе
         :return:
         """
-        return self.capacity - sum(self.items.values())
+        return self.capacity - sum(self._items.values())
 
-    def get_items(self):
-        """получает все товары на складе"""
-        return self.items
+    def get_items(self) -> dict:
+        """Получает все товары на складе"""
+        return self._items
 
-    def get_unique_items_count(self, items):
-        """возвращает количество уникальных товаров."""
-        all_items = [item for item in items]
+    def get_unique_items_count(self, _items: dict) -> int:
+        """Возвращает количество уникальных товаров."""
+        all_items = [item for item in _items]
         unique_items = set(all_items)
         return len(unique_items)
 
-    def print_add_message(self, item):
-        """выводит сообщение о добавлении товара на склад(в список всех товаров)"""
+    def print_add_message(self, item: str) -> None:
+        """Выводит сообщение о добавлении товара на склад(в список всех товаров)"""
         return print(f"Товар '{item}' добавлен на склад\n")
 
-    def print_low_capacity_message(self, item):
+    def print_low_capacity_message(self, item: str) -> None:
         """
-        выводит сообщение о недостаточном свободном месте на складе (capacity)
+        Выводит сообщение о недостаточном свободном месте на складе (capacity)
         :param item: название товара
         :return:
         """
@@ -99,50 +96,51 @@ class Store(Storage):
 
 
 class Shop(Store):
-    def __init__(self, item_limit=5):
+    def __init__(self, item_limit: int = 5):
         super().__init__()
-        self.items = {}
+        self._items = {}
         self.capacity = 20
         self._item_limit = item_limit
 
     def get_item_limit(self):
         return self._item_limit
 
-    def add(self, item, quantity):
-        items = self.items
-        if self.get_unique_items_count(items) < self._item_limit:
+    def add(self, item: str, quantity: int) -> None:
+        _items = self._items
+        if self.get_unique_items_count(_items) < self._item_limit:
             super().add(item, quantity)
         else:
             print(f"Достингут лимит по количеству товаров.'")
 
-    def remove(self, item, quantity):
+    def remove(self, item: str, quantity: int) -> None:
         super().remove(item, quantity)
 
-    def get_free_space(self):
+    def get_free_space(self) -> int:
         return super().get_free_space()
 
-    def get_items(self):
+    def get_items(self) -> dict:
         return super().get_items()
 
-    def get_unique_items_count(self, items):
-        """возвращает количество уникальных товаров."""
-        # items = self.items
-        return super().get_unique_items_count(items)
+    def get_unique_items_count(self, _items: dict) -> int:
+        """Возвращает количество уникальных товаров."""
+        # _items = self._items
+        return super().get_unique_items_count(_items)
 
-    def print_add_message(self, item):
-        """выводит сообщение о добавлении товара на склад(в список всех товаров)"""
+    def print_add_message(self, item: str) -> None:
+        """Выводит сообщение о добавлении товара на склад(в список всех товаров)"""
         return super().print_add_message(item)
 
-    def print_low_capacity_message(self, item):
+    def print_low_capacity_message(self, item: str) -> None:
         """
-        выводит сообщение о недостаточном свободном месте на складе (capacity)
+        Выводит сообщение о недостаточном свободном месте на складе (capacity)
         :param item: название товара
         :return:
         """
         return super().print_low_capacity_message(item)
 
+
 class Request:
-    def __init__(self, input_):
+    def __init__(self, input_: str) -> None:
         list_of_words = self.split_input(input_)
 
         self.from_ = list_of_words[4]
@@ -151,5 +149,6 @@ class Request:
         self.quantity = int(list_of_words[1])
         self.item = list_of_words[2]
 
-    def split_input(self, input_):
+    def split_input(self, input_: str) -> list:
         return input_.split(" ")
+
